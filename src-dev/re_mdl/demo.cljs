2a4470d2b8b298b4f015c34c073e37e22ab59c10
Milt Reder <milt@anestuary.com>
2015-11-06 15:46:16 -0500

    added all components to core

--- src-dev/re_mdl/demo.cljs
+++ src-dev/re_mdl/demo.cljs
@@ -1,19 +1,7 @@
 (ns ^:figwheel-always re-mdl.demo
   (:require
-   [re-mdl.core]
-   [reagent.core :as r]
-   [re-mdl.components.badge :as badge]
-   [re-mdl.components.button :as button]
-   [re-mdl.components.card :as card]
-   [re-mdl.components.grid :as grid]
-   [re-mdl.components.layout :as layout]
-   [re-mdl.components.loading :as loading]
-   [re-mdl.components.menu :as menu]
-   [re-mdl.components.slider :as slider]
-   [re-mdl.components.toggle :as toggle]
-   [re-mdl.components.table :as table]
-   [re-mdl.components.textfield :as textfield]
-   [re-mdl.components.tooltip :as tooltip]))
+   [re-mdl.core :as mdl]
+   [reagent.core :as r]))
 
 (enable-console-print!)
 
@@ -24,21 +12,21 @@
 
 (defn badge-demo []
   [:div.badge-demo
-   [badge/badge
+   [mdl/badge
     :badge-label "3"
     :child
     "Badge"]
-   [badge/badge
+   [mdl/badge
     :badge-label "3"
     :no-background? true
     :child
     "No Background"]
-   [badge/badge
+   [mdl/badge
     :badge-label "3"
     :overlap? true
     :child
     "Overlap"]
-   [badge/badge
+   [mdl/badge
     :badge-label "♥"
     :icon? true
     :child
@@ -46,33 +34,33 @@
 
 (defn button-demo []
   [:div.button-demo
-   [button/button
+   [mdl/button
     :label "Button"]
-   [button/button
+   [mdl/button
     :primary? true
     :label "Primary"]
-   [button/button
+   [mdl/button
     :raised? true
     :label "Raised"]
-   [button/button
+   [mdl/button
     :fab? true
     :label "Fab"]
-   [button/button
+   [mdl/button
     :mini-fab? true
     :label [:i.material-icons "add"]]
-   [button/button
+   [mdl/button
     :ripple-effect? true
     :label "Ripple"]])
 
 (defn card-demo []
-  [card/card
+  [mdl/card
    :shadow 2
    :children
-   [[card/title
+   [[mdl/card-title
      :expand? true
      :header :h4
      :text "Card"]
-    [card/actions
+    [mdl/card-actions
      :border? true
      :children
      [[:a.mdl-button.mdl-button--colored.mdl-js-button.mdl-js-ripple-effect
@@ -80,32 +68,32 @@
       ]]]])
 
 (defn grid-demo [& kids]
-  [grid/grid
+  [mdl/grid
    :children
    (into []
          (for [kid kids]
-           [grid/cell
+           [mdl/cell
             :children
             [kid]]))])
 
 (defn mega-footer-demo []
-  [layout/mega-footer
+  [mdl/mega-footer
    :logo "Title"
    :middle
-   [[layout/mega-drop-down
+   [[mdl/mega-footer-dropdown
      :heading "Features"
      :children
      [[:a {:href "#"} "About"]
       [:a {:href "#"} "Terms"]
       [:a {:href "#"} "Partners"]
       [:a {:href "#"} "Updates"]]]
-    [layout/mega-drop-down
+    [mdl/mega-footer-dropdown
      :heading "Details"
      :children
      [[:a {:href "#"} "Specs"]
       [:a {:href "#"} "Tools"]
       [:a {:href "#"} "Resources"]]]
-    [layout/mega-drop-down
+    [mdl/mega-footer-dropdown
      :heading "Technology"
      :children
      [[:a {:href "#"} "How it works"]
@@ -113,45 +101,45 @@
       [:a {:href "#"} "Usage"]
       [:a {:href "#"} "Products"]
       [:a {:href "#"} "Contracts"]]]
-    [layout/mega-drop-down
+    [mdl/mega-footer-dropdown
      :heading "FAQ"
      :children
      [[:a {:href "#"} "Questions"]
       [:a {:href "#"} "Answers"]
       [:a {:href "#"} "Contact"]]]]
    :bottom
-   [[layout/mega-link-list
+   [[mdl/mega-footer-link-list
     :children
     [[:a {:href "#"} "Help"]
      [:a {:href "#"} "Privacy & Terms"]]]]])
 
 (defn mini-footer-demo []
-  [layout/mini-footer
+  [mdl/mini-footer
    :logo "Title"
    :left
-   [[layout/mini-link-list
+   [[mdl/mini-footer-link-list
      :children
      [[:a {:href "#"} "Help"]
       [:a {:href "#"} "Privacy & Terms"]]]]])
 
 (defn tab-demo []
-  [layout/tabs
+  [mdl/tabs
    :children
-   [[layout/tab-bar
+   [[mdl/tab-bar
      :children
-     [[layout/tab
+     [[mdl/tab
       :href "#starks-panel"
        :is-active? true
        :content "Starks"]
-      [layout/tab
+      [mdl/tab
        :href "#lannisters-panel"
        :is-active? true
        :content "Lannisters"]
-      [layout/tab
+      [mdl/tab
        :href "#targaryens-panel"
        :is-active? true
        :content "Targaryens"]]]
-    [layout/tabs-panel
+    [mdl/tabs-panel
      :is-active? true
      :id "starks-panel"
      :children
@@ -159,14 +147,14 @@
        [:li "Eddard"]
        [:li "Catelyn"]
        [:li "Rob"]]]]
-    [layout/tabs-panel
+    [mdl/tabs-panel
      :id "lannisters-panel"
      :children
      [[:ul
        [:li "Tywin"]
        [:li "Cersei"]
        [:li "Jamie"]]]]
-    [layout/tabs-panel
+    [mdl/tabs-panel
      :id "targaryens-panel"
      :children
      [[:ul
@@ -174,37 +162,37 @@
        [:li "Danerys"]]]]]])
 
 (defn demo-layout [& {:keys [demo-map current-demo-ra children]}]
-  [layout/layout
+  [mdl/layout
    :fixed-header? true
    :children
-   [[layout/header
+   [[mdl/layout-header
      :children
-     [[layout/header-row
+     [[mdl/layout-header-row
        :children
-       [[layout/title :label "Material Design Lite"]
-        [layout/spacer]
-        [layout/nav
+       [[mdl/layout-title :label "Material Design Lite"]
+        [mdl/layout-spacer]
+        [mdl/layout-nav
          :children
          (into []
                (for [demo (keys demo-map)]
-                 [layout/nav-link
+                 [mdl/layout-nav-link
                   :href "#"
                   :content (name demo)
                   :on-click (fn [e]
                               (reset! current-demo-ra demo))]))]]]]]
-    [layout/drawer
+    [mdl/layout-drawer
      :children
-     [[layout/title :label "Material Design Lite"]
-      [layout/nav
+     [[mdl/layout-title :label "Material Design Lite"]
+      [mdl/layout-nav
        :children
        (into []
              (for [demo (keys demo-map)]
-               [layout/nav-link
+               [mdl/layout-nav-link
                 :href "#"
                 :content (name demo)
                 :on-click (fn [e]
                             (reset! current-demo-ra demo))]))]]]
-    [layout/content
+    [mdl/layout-content
      :children children]]])
 
 
@@ -212,34 +200,34 @@
   (let [pct-done (r/atom 0)]
     (fn []
       [:div.loading-demo
-       [loading/progress
+       [mdl/loading-progress
         :indeterminate? true]
-       [button/button
+       [mdl/button
        :mini-fab? true
        :on-click #(swap! pct-done inc)
        :label [:i.material-icons "add"]]
-      [loading/progress
+      [mdl/loading-progress
        :model @pct-done
        ]])))
 
 (defn loading-spinner-demo []
-  [loading/spinner
+  [mdl/loading-spinner
    :is-active? true])
 
 (defn menu-demo []
   [:div.menu-demo
-   [button/button
+   [mdl/button
     :id "menu-speed"
     :icon "more_vert"]
-   [menu/menu
+   [mdl/menu
     :for "menu-speed"
     :ripple-effect? true
     :children
-    [[menu/item
+    [[mdl/menu-item
       :label "Fast"]
-     [menu/item
+     [mdl/menu-item
       :label "Medium"]
-     [menu/item
+     [mdl/menu-item
       :label "Slow"]]]])
 
 
@@ -247,7 +235,7 @@
   [:div.slider-demo
    [:p
     {:width "300px"}
-    [slider/slider
+    [mdl/slider
      :id "foo"
      :init-val 4
      :min 0
@@ -258,22 +246,22 @@
 
 (defn toggles-demo []
   [:div.toggles-demo
-   [toggle/checkbox
+   [mdl/toggle-checkbox
     :id "checkbox-demo"
     :checked? true
     :label "Checkbox"
     :handler-fn #(print "checkbox: " %)]
-   [toggle/radios
+   [mdl/toggle-radios
     :name "radio-demo"
     :handler-fn #(print "radio: " %)
     :choices
     [[:foo "Foo"] [:bar "Bar"] [:baz "Baz"]]]
-   [toggle/icon-toggle
+   [mdl/toggle-icon-toggle
     :id "icon-toggle-demo"
     :labels ["format_bold" "format_italic"]
     :ripple-effect? true
     :handler-fn #(print (str "icon-toggle: " %))]
-   [toggle/switch
+   [mdl/toggle-switch
     :id "switch-demo"
     :label "On/Off"
     :ripple-effect? true
@@ -281,7 +269,7 @@
 
 (defn table-demo []
   [:div.table-demo
-   [table/table
+   [mdl/table
     :id "table-demo"
     :selectable? true
     :headers
@@ -295,39 +283,39 @@
 (defn text-field-demo []
   [:div.text-field-demo
    [grid-demo
-    [textfield/textfield
+    [mdl/textfield
     :label "Basic Text Field"
     :id "text-field-demo-basic"
     :handler-fn #(print "basic text field: " %)]
-   [textfield/textfield
+   [mdl/textfield
     :label "With Init Val"
     :id "text-field-demo-init-val"
     :handler-fn #(print "init-val text field: " %)
     :init-val "Foo"]
-   [textfield/textfield
+   [mdl/textfield
     :label "Basic Text Area"
     :id "text-field-demo-basic-textarea"
     :type :textarea
     :rows 2
     :handler-fn #(print "basic text area: " %)]
-   [textfield/textfield
+   [mdl/textfield
     :id "text-field-demo-numeric"
     :label "Numeric validation"
     :pattern "-?[0-9]*(\\.[0-9]+)?"
     :invalid-message "Must be a number, yo."
     :handler-fn #(print "numeric text field: " %)]
-   [textfield/textfield
+   [mdl/textfield
     :label "Floating Label"
     :id "text-field-demo-float"
     :handler-fn #(print "floating field: " %)
     :floating-label? true]
-   [textfield/textfield
+   [mdl/textfield
     :label "Expandable"
     :id "text-field-demo-expandable"
     :handler-fn #(print "expandable: " %)
     :expandable? true
     :expand-icon "search"]
-   [textfield/textfield
+   [mdl/textfield
     :label "Floating Multiline Textarea"
     :id "text-field-demo-multiline-textarea"
     :type :textarea
@@ -341,7 +329,7 @@
     [:div
      {:id "tooltip-demo-div"}
      "A Div"]
-    [tooltip/tooltip
+    [mdl/tooltip
      :for "tooltip-demo-div"
      :children ["Can have tooltips"]]
 
@@ -349,14 +337,14 @@
      {:id "tooltip-demo-p"}
      "A p"]
 
-    [tooltip/tooltip
+    [mdl/tooltip
      :for "tooltip-demo-p"
      :children ["Can also have tooltips"]]
 
     [:span
      {:id "tooltip-demo-span"}
      "A span"]
-    [tooltip/tooltip
+    [mdl/tooltip
      :for "tooltip-demo-span"
      :large? true
      :children ["Giant tooltip!"]]]])
--- src/re_mdl/core.cljs
+++ src/re_mdl/core.cljs
@@ -1,7 +1,141 @@
-(ns  ^:figwheel-always re-mdl.core
+(ns re-mdl.core
   (:require
    [cljsjs.material]
+   [re-mdl.components.badge :as badge]
+   [re-mdl.components.button :as button]
+   [re-mdl.components.card :as card]
+   [re-mdl.components.grid :as grid]
+   [re-mdl.components.layout :as layout]
+   [re-mdl.components.loading :as loading]
+   [re-mdl.components.menu :as menu]
+   [re-mdl.components.slider :as slider]
+   [re-mdl.components.toggle :as toggle]
+   [re-mdl.components.table :as table]
+   [re-mdl.components.textfield :as textfield]
+   [re-mdl.components.tooltip :as tooltip]))
 
-   [re-mdl.components.badge :as badge]))
 
 (def badge badge/badge)
+
+(def button button/button)
+
+;; card
+(def card-title card/title)
+
+(def card-media card/media)
+
+(def card-supporting-text card/supporting-text)
+
+(def card-actions card/actions)
+
+(def card card/card)
+
+
+;; grid
+
+(def grid grid/grid)
+
+(def cell grid/cell)
+
+
+;; layout
+
+(def layout layout/layout)
+
+(def layout-title layout/title)
+
+(def layout-spacer layout/spacer)
+
+(def layout-header layout/header)
+
+(def layout-icon layout/icon)
+
+(def layout-header-row layout/header-row)
+
+(def layout-drawer layout/drawer)
+
+(def layout-content layout/content)
+
+(def layout-nav layout/nav)
+
+(def layout-nav-link layout/nav-link)
+
+(def layout-tab-bar layout/layout-tab-bar)
+
+(def layout-tab layout/layout-tab)
+
+(def layout-tab-panel layout/layout-tab-panel)
+
+;; tabs (part of layout, but whatever)
+
+(def tab-bar layout/tab-bar)
+
+(def tabs layout/tabs)
+
+(def tab layout/tab)
+
+(def tabs-panel layout/tabs-panel)
+
+;; footers
+
+(def mega-footer-social-btn layout/mega-social-btn)
+
+(def mega-footer-link-list layout/mega-link-list)
+
+(def mega-footer-dropdown layout/mega-drop-down)
+
+(def mega-footer layout/mega-footer)
+
+
+(def mini-footer-social-btn layout/mini-social-btn)
+
+(def mini-footer-link-list layout/mini-link-list)
+
+(def mini-footer layout/mini-footer)
+
+
+;; loading
+
+(def loading-progress loading/progress)
+
+(def loading-spinner loading/spinner)
+
+
+;; Menu
+
+(def menu-item menu/item)
+
+(def menu menu/menu)
+
+
+;; slider
+
+(def slider slider/slider)
+
+
+;; table
+
+(def table table/table)
+
+
+;; textfield
+
+(def textfield textfield/textfield)
+
+
+;; toggle
+
+(def toggle-checkbox toggle/checkbox)
+
+(def toggle-radio toggle/radio)
+
+(def toggle-radios toggle/radios)
+
+(def toggle-icon-toggle toggle/icon-toggle)
+
+(def toggle-switch toggle/switch)
+
+
+;; tooltip
+
+(def tooltip tooltip/tooltip)
