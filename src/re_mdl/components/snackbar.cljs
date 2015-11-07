4f40b70f24cae73a4a277313d58af3baa099f6c4
Milt Reder <milt@anestuary.com>
2015-11-06 20:11:50 -0500

    removed print

--- src/re_mdl/components/snackbar.cljs
+++ src/re_mdl/components/snackbar.cljs
@@ -3,7 +3,6 @@
             [re-mdl.util :refer [wrap-mdl]]))
 
 (defn toast! [& {:keys [message action-handler action-text timeout] :as args}]
-  (print args)
   (-> (.querySelector js/document ".mdl-js-snackbar")
       .-MaterialSnackbar
       (.showSnackbar (clj->js args))))
